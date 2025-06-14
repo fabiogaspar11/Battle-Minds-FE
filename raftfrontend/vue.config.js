cc4066d6307fe120fa408245a032bdf4c47bc027
Fábio Gaspar <fabiocordeirogaspar@gmail.com>
2025-06-14 17:33:16 +0100

    a

--- raftfrontend/vue.config.js
+++ raftfrontend/vue.config.js
@@ -1,5 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/Battle-Minds/',
-})
+module.exports = {
+  publicPath: '/Battle-Minds-FE/',
+  
+};
